28969df5f80d478eb1abb613e69151052854306f
Jonathan Penny <jtpenny@gmail.com>
2017-01-20 14:42:57 -0600

    Adding Laravel Cookbook

---
+++ laravel/metadata.rb
@@ -0,0 +1,7 @@
+name        "laravel"
+description "Makes Laravel Work"
+maintainer  "Jonathan Penny"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "mod_php5_apache2"
---
+++ laravel/recipes/deploy.rb
@@ -0,0 +1,12 @@
+node[:deploy].each do |application, deploy|
+  if defined? deploy["laravel"] && deploy["laravel"]] == "true"
+    bash 'laravel-deploy' do
+      code <<-EOH
+      cp .env.example .env
+      /usr/bin/php artisan key:generate
+      chmod -R 777 storage/
+      EOH
+      cwd deploy[:current_path]
+    end
+  end  
+end
